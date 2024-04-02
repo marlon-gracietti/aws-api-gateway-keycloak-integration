7acf698ca1041df4c4df04ead283c6ede4ab2562
Marlon Gracietti de Amorim <marlon.gracietti@levva.io>
2024-04-02 14:15:42 -0300

    remove old comments

--- keycloak-request-token/lambda_function.py
+++ keycloak-request-token/lambda_function.py
@@ -10,12 +10,6 @@ def get_parameter(name, with_decryption=False):
     response = ssm.get_parameter(Name=name, WithDecryption=with_decryption)
     return response['Parameter']['Value']
 
-# Configurações do Keycloak
-# KEYCLOAK_URL = os.getenv('KEYCLOAK_BASE_URL')
-# KEYCLOAK_REALM = os.getenv('KEYCLOAK_REALM_NAME')
-# KEYCLOAK_CLIENT_ID = os.getenv('KEYCLOAK_CLIENT_ID')
-# KEYCLOAK_CLIENT_SECRET = os.getenv('KEYCLOAK_CLIENT_SECRET')
-
 KEYCLOAK_URL = get_parameter('/keycloak/nhub/base_url')
 KEYCLOAK_REALM = get_parameter('/keycloak/nhub/realm_name')
 KEYCLOAK_CLIENT_ID = get_parameter('/keycloak/nhub/client_id')
